c0cc23511f8f580ff490a40f0d4132e4d5742192
Roz Isaacson <rozisaacson@users.noreply.github.com>
2016-01-09 07:29:56 -0600

    Add basic movie scheduling rules

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import csv
 import os
 
 from collections import namedtuple
+from datetime import datetime, timedelta
+
+import pyyaml
 
 TESTFILENAME = 'movies.csv'
 VALIDFILEEXT = 'csv'
@@ -26,6 +29,53 @@ def get_movies(filename):
 
     return movies
 
+def open_time():
+    return datetime(2016, 1, 8, 12, 0)
+
+def close_time():
+    return datetime(2016, 1, 8, 23, 0)
+
+def get_the_time(h, m):
+    return datetime(2016, 1, 8, h, m)
+
+def parse_real_run_time(the_time):
+    x, y = the_time.split(":")
+    return int(x), int(y)
+
+def parse_run_time(close):
+    x, y = close.split(':')
+    #new_y = y[:]
+    z = int(y)%10
+
+    if z < 5:
+        new_y = int(y) + 5 - z
+        new_x = int(x)
+
+        if int(y) < 5:
+            new_y = 5
+
+    elif 5 < z < 10:
+        new_x = int(x)
+        new_y = int(y) + 10 - z
+        if new_y == 60:
+            new_x = int(x)+1
+            new_y = 0
+
+    #print (x, y, new_x, new_y, z)
+
+    return new_x, new_y
+#    print (x)
+
+def to_datetime_object(date_string, date_format):
+    s = datetime.strptime(date_string, date_format)
+    return s
+
+def go_back(movie, close):
+    h, m = parse_run_time(movie)
+    #print(h, m)
+    goBack = close - timedelta(hours=h, minutes=m)
+    #print(goBack)
+    return goBack
 
 def valid_file_ext(filename):
     """ Check if the filename has a valid extension
@@ -39,9 +89,48 @@ def valid_file_ext(filename):
 
     return filename
 
+def format_movie_over_time(movie_over):
+    return datetime.strftime(movie_over, '%I:%M %p')
+
+def generate_schedule(movies):
+    time_compare = open_time()
+
+    show_time = close_time()
+
+    schedule = []
+
+    for movie in movies:
+        print (movie)
+        h_r , m_r = parse_real_run_time(movie.run_time)
+        print (h_r, m_r)
+        h, m = parse_run_time(movie.run_time)
+        show_time = go_back(movie.run_time, show_time)
+        schedule.append((datetime.strftime(show_time, '%I:%M %p'), format_movie_over_time(show_time + timedelta(hours=h_r, minutes=m_r))))
+        while show_time > time_compare:
+            show_time = show_time - timedelta(minutes=35)
+            show_time = go_back(movie.run_time, show_time)
+            if show_time > time_compare:
+                #print(show_time, time_compare)
+                schedule.append((datetime.strftime(show_time, '%I:%M %p'),
+                          format_movie_over_time(show_time + timedelta(hours=h_r, minutes=m_r))))
+
+        print(schedule[::-1])
+        time_compare = open_time()
+        show_time = close_time()
+        schedule.clear()
+
+    return schedule[::-1]
+
+
+
+def print_schedule(schedule):
+    pass
+
 
 def main(filename):
     movies = get_movies(filename)
+
+    generate_schedule(movies)
     print("Successfully processed {}".format(filename))
 
 
