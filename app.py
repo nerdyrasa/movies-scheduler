5b0fb66180099c4f734f2835d7b4a110b88e2309
Roz Isaacson <rozisaacson@users.noreply.github.com>
2016-01-09 10:19:46 -0600

    Add config file

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import argparse
 import csv
 import os
-
 from collections import namedtuple
 from datetime import datetime, timedelta
-
-import pyyaml
+import yaml
 
 TESTFILENAME = 'movies.csv'
 VALIDFILEEXT = 'csv'
-fields =  ('movie_title', 'release_year', 'mpaa_rating', 'run_time')
+FIELDS = ('movie_title', 'release_year', 'mpaa_rating', 'run_time')
+WEEKEND = ['Friday', 'Saturday', 'Sunday']
 
 
 def get_movies(filename):
@@ -17,35 +16,40 @@ def get_movies(filename):
     :param filename: name of movie file entered in command line
     :return: list of movies
     """
-    Movie = namedtuple('Movie', fields)
+    Movie = namedtuple('Movie', FIELDS)
     movies = []
 
-    with open(filename, newline="" ) as infile:
+    with open(filename, newline="") as infile:
         reader = csv.reader(infile)
         header = next(reader)
-        Movie = namedtuple("Movie", fields)
+        Movie = namedtuple("Movie", FIELDS)
         for data in map(Movie._make, reader):
             movies.append(data)
 
     return movies
 
+
 def open_time():
     return datetime(2016, 1, 8, 12, 0)
+
 
 def close_time():
     return datetime(2016, 1, 8, 23, 0)
 
+
 def get_the_time(h, m):
     return datetime(2016, 1, 8, h, m)
+
 
 def parse_real_run_time(the_time):
     x, y = the_time.split(":")
     return int(x), int(y)
 
+
 def parse_run_time(close):
     x, y = close.split(':')
-    #new_y = y[:]
-    z = int(y)%10
+    # new_y = y[:]
+    z = int(y) % 10
 
     if z < 5:
         new_y = int(y) + 5 - z
@@ -58,24 +62,26 @@ def parse_run_time(close):
         new_x = int(x)
         new_y = int(y) + 10 - z
         if new_y == 60:
-            new_x = int(x)+1
+            new_x = int(x) + 1
             new_y = 0
 
-    #print (x, y, new_x, new_y, z)
+    # print (x, y, new_x, new_y, z)
 
     return new_x, new_y
-#    print (x)
+
 
 def to_datetime_object(date_string, date_format):
     s = datetime.strptime(date_string, date_format)
     return s
 
+
 def go_back(movie, close):
     h, m = parse_run_time(movie)
-    #print(h, m)
+    # print(h, m)
     goBack = close - timedelta(hours=h, minutes=m)
-    #print(goBack)
+    # print(goBack)
     return goBack
+
 
 def valid_file_ext(filename):
     """ Check if the filename has a valid extension
@@ -85,12 +91,14 @@ def valid_file_ext(filename):
     ext = os.path.splitext(filename)[1][1:]
 
     if ext != VALIDFILEEXT:
-       parser.error("Please submit a csv file")
+        parser.error("Please submit a csv file")
 
     return filename
 
+
 def format_movie_over_time(movie_over):
     return datetime.strftime(movie_over, '%I:%M %p')
+
 
 def generate_schedule(movies):
     time_compare = open_time()
@@ -100,19 +108,20 @@ def generate_schedule(movies):
     schedule = []
 
     for movie in movies:
-        print (movie)
-        h_r , m_r = parse_real_run_time(movie.run_time)
-        print (h_r, m_r)
+        print(movie)
+        h_r, m_r = parse_real_run_time(movie.run_time)
+        print(h_r, m_r)
         h, m = parse_run_time(movie.run_time)
         show_time = go_back(movie.run_time, show_time)
-        schedule.append((datetime.strftime(show_time, '%I:%M %p'), format_movie_over_time(show_time + timedelta(hours=h_r, minutes=m_r))))
+        schedule.append((datetime.strftime(show_time, '%I:%M %p'),
+                         format_movie_over_time(show_time + timedelta(hours=h_r, minutes=m_r))))
         while show_time > time_compare:
             show_time = show_time - timedelta(minutes=35)
             show_time = go_back(movie.run_time, show_time)
             if show_time > time_compare:
-                #print(show_time, time_compare)
+                # print(show_time, time_compare)
                 schedule.append((datetime.strftime(show_time, '%I:%M %p'),
-                          format_movie_over_time(show_time + timedelta(hours=h_r, minutes=m_r))))
+                                 format_movie_over_time(show_time + timedelta(hours=h_r, minutes=m_r))))
 
         print(schedule[::-1])
         time_compare = open_time()
@@ -122,9 +131,18 @@ def generate_schedule(movies):
     return schedule[::-1]
 
 
-
 def print_schedule(schedule):
     pass
+
+
+def load_config():
+    with open("app.yaml") as stream:
+        return yaml.load(stream)
+
+def load_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=lambda s: valid_file_ext(s.lower()))
+    return parser.parse_args()
 
 
 def main(filename):
@@ -135,7 +153,6 @@ def main(filename):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filename',type=lambda s:valid_file_ext(s.lower()))
-    args = parser.parse_args()
+    args = load_args()
+    config = load_config()
     main(args.filename)
